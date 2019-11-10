5ffa607bf363cb3a88dd631aa37af189a2e03973
tony <793715596@qq.com>
2019-11-10 17:41:56 +0800

    登录POST请求方式修改

--- src/main/java/com/pyh/bookservice/control/LoginController.java
+++ src/main/java/com/pyh/bookservice/control/LoginController.java
@@ -8,6 +8,7 @@ package com.pyh.bookservice.control;
 import com.pyh.bookservice.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
@@ -17,7 +18,7 @@ public class LoginController {
     @Autowired
     LoginService loginService;
 
-    @GetMapping(value = "/login")
+    @PostMapping(value = "/login")
     public Map longin(@RequestParam String pid, @RequestParam String ps){
         return loginService.login(pid,ps);
     }
